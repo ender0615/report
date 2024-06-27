4acb433de8ca5390fd87cdec61d6a7c619cc2a31
Ратмир Хашагульгов <ender0615@mail.ru>
2024-06-27 06:02:03 +0300

    add

---
+++ backend/cats/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Cat
+
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'birth_year')
+
+
+admin.site.register(Cat, CatAdmin)
